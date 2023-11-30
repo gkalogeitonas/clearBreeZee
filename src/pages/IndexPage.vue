3bba75486899465fe489ae545caf85eee27a559b
Giorgos Kalogeitonas <gkalog2@gmail.com>
2023-12-01 01:24:25 +0200

    WIP

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,28 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex column">
+    <div class="col q-pt-lg  q-px-md">
+      <q-input color="white" text-color="white" label-color="white" outlined v-model="search" placeholder="Search" >
+        <template v-slot:append>
+          <q-icon name="search" color="white" />
+        </template>
+      </q-input>
+    </div>
 
+    <div class="col text-white text-center">
+      <div class="text-h4 text-weight-light">
+        Naxos
+      </div>
+      <div class="text-h6 text-weight-light">
+        Rain
+      </div>
+      <div class="text-h1 text-weight-thin q-my-lg">
+        <span>8</span>
+        <span class="text-small">°</span>C
+      </div>
+    </div>
+
+    <div class="col" id="footer">
+    </div>
   </q-page>
 </template>
 
@@ -15,5 +37,8 @@ export default defineComponent({
 <style lang="scss">
   .q-page{
     background: linear-gradient(to bottom, #13648a, #267881);
+  }
+  #footer{
+    flex: 0 0 100px;
   }
 </style>
