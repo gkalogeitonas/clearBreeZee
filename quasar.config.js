827e041d0ec123e12505f59dee505066ea35af5a
Giorgos Kalogeitonas <gkalog2@gmail.com>
2023-12-31 12:57:17 +0200

    install pinia and make an example in personal-info page

--- quasar.config.js
+++ quasar.config.js
@@ -45,7 +45,7 @@ module.exports = configure(function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
-
+      'pinia'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
