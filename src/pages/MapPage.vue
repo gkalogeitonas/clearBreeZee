7eaee545c8699dfc793b4d6f8893e59d53b34d6b
Giorgos Kalogeitonas <gkalog2@gmail.com>
2024-01-07 23:59:54 +0200

    listen to user click to change the center also get's data from the ne location

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -4,49 +4,55 @@
         <div class="text-h4 text-weight-light">
           {{ weatherData.name }}
         </div>
-        <div class="text-h6 text-weight-light">
+        <!-- <div class="text-h6 text-weight-light">
           {{weatherData.weather[0].main}}
-        </div>
-        <div class="text-h2 text-weight-thin q-my-lg">
+        </div> -->
+        <!-- <div class="text-h2 text-weight-thin q-my-lg">
           <span>{{Math.round(weatherData.main.temp,2)}}</span>
           <span class="text-small">°</span>C
-        </div>
+        </div> -->
       </div>
       <div v-if="pollutionData" class="col text-black text-center">
         <div class="aqi-circle">
           <div class="text-h2">{{ pollutionData.main.aqi }}</div>
           <div class="text-caption">AQI</div>
         </div>
-        <div class="text-h6 text-weight-light">
-          NO: {{ pollutionData.components.no }} μg/m3
-        </div>
-        <div class="text-h6 text-weight-light">
-          NO2: {{ pollutionData.components.no2 }} μg/m3
-        </div>
-        <div class="text-h6 text-weight-light">
-          O3: {{ pollutionData.components.o3 }} μg/m3
-        </div>
-        <div class="text-h6 text-weight-light">
-          SO2: {{ pollutionData.components.so2 }} μg/m3
-        </div>
-        <div class="text-h6 text-weight-light">
-          PM2.5: {{ pollutionData.components.pm2_5 }} μg/m3
-        </div>
-        <div class="text-h6 text-weight-light">
-          PM10: {{ pollutionData.components.pm10 }} μg/m3
-        </div>
-        <div class="text-h6 text-weight-light">
-          NH3: {{ pollutionData.components.nh3 }} μg/m3
+        <div style="display: flex; flex-wrap: wrap;">
+          <div style="flex: 1;">
+            <div class="text-h6 text-weight-light">
+              NO: {{ pollutionData.components.no }} μg/m3
+            </div>
+            <div class="text-h6 text-weight-light">
+              NO2: {{ pollutionData.components.no2 }} μg/m3
+            </div>
+            <div class="text-h6 text-weight-light">
+              O3: {{ pollutionData.components.o3 }} μg/m3
+            </div>
+            <div class="text-h6 text-weight-light">
+              SO2: {{ pollutionData.components.so2 }} μg/m3
+            </div>
+          </div>
+          <div style="flex: 1;">
+            <div class="text-h6 text-weight-light">
+              PM2.5: {{ pollutionData.components.pm2_5 }} μg/m3
+            </div>
+            <div class="text-h6 text-weight-light">
+              PM10: {{ pollutionData.components.pm10 }} μg/m3
+            </div>
+            <div class="text-h6 text-weight-light">
+              NH3: {{ pollutionData.components.nh3 }} μg/m3
+            </div>
+          </div>
         </div>
       </div>
     </template>
-  <GoogleMap :api-key="googleMapsApiKey" style="width: 100%; height: 500px" :center="center" :zoom="15">
+  <GoogleMap @click="handleMapClick" :api-key="googleMapsApiKey" style="width: 100%; height: 500px" :center="center" :zoom="15">
     <MapMarker :options="{ position: center }" />
   </GoogleMap>
 </template>
 
 <script>
-import { ref, onMounted, defineComponent } from 'vue'
+import { ref, watch, onMounted, defineComponent } from 'vue'
 // import { useStore } from 'stores/store'
 import { GoogleMap, Marker as MapMarker } from 'vue3-google-map'
 import { WeatherApi } from 'src/services/weatherApi.js'
@@ -74,13 +80,26 @@ export default defineComponent({
         })
       }
     })
-    console.log(weatherData.value)
+    // Define the handleMapClick function
+    const handleMapClick = (event) => {
+      center.value = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng()
+      }
+      console.log(center.value)
+    }
+    console.log(center)
+    watch(center, async (newCenter) => {
+      weatherData.value = await weatherApi.getWeatherByCoords(newCenter.lat, newCenter.lng)
+      pollutionData.value = await weatherApi.getPollutionByCoords(newCenter.lat, newCenter.lng)
+    }, { immediate: true })
     return {
       // googleMapsApiKey: store.googleMapsApiKey,
       googleMapsApiKey: process.env.googleMapsApiKey,
       center,
       weatherData,
-      pollutionData
+      pollutionData,
+      handleMapClick
     }
   }
 })
