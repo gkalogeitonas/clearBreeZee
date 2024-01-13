3fff6a0ac3d542bf9f856cc2ee274ed4f53c7260
Giorgos Kalogeitonas <gkalog2@gmail.com>
2024-01-13 20:15:41 +0200

    Update project name to clearBreeZee

--- quasar.config.js
+++ quasar.config.js
@@ -213,7 +213,7 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'air-quality-app'
+        appId: 'clearBreeZee'
       }
     },
 
