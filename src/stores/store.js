ec0ccd745f8a9237a3899bc8fe0ff4201920dd22
Giorgos Kalogeitonas <gkalog2@gmail.com>
2024-01-14 23:04:20 +0200

    works the local storage with home obj

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,21 +1,33 @@
-// store.js
 import { defineStore } from 'pinia'
+import { toRaw } from 'vue'
 
 export const useStore = defineStore({
   id: 'main',
-  state: () => ({
-    home: JSON.parse(localStorage.getItem('home')),
-    work: localStorage.getItem('work') || ''
-  }),
+  state: () => {
+    const home = JSON.parse(localStorage.getItem('home')) || {
+      address: '',
+      lat: '',
+      lng: ''
+    }
+    const work = JSON.parse(localStorage.getItem('work')) || {
+      address: '',
+      lat: '',
+      lng: ''
+    }
+
+    return {
+      home,
+      work
+    }
+  },
   actions: {
-    setHome (url) {
-      this.home = url
-      console.log(JSON.stringify(url))
-      localStorage.setItem('home', JSON.stringify(url))
+    setHome (home) {
+      this.home = home
+      localStorage.setItem('home', JSON.stringify(toRaw(home.value)))
     },
-    setWork (url) {
-      this.work = url
-      localStorage.setItem('work', url)
+    setWork (work) {
+      this.work = work
+      localStorage.setItem('work', JSON.stringify(toRaw(work.value)))
     }
   }
 })
