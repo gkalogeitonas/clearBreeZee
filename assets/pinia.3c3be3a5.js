30cc9f3344f1d2ce1ab5e64a7f2c86b70928d9c9
gkalogeitonas <gkalogeitonas@users.noreply.github.com>
2024-01-19 19:50:57 +0000

    deploy: 8f67bbffac27771e80735b08c66a954f7f5fd1c2

---
+++ assets/pinia.3c3be3a5.js
@@ -0,0 +1,1 @@
+import{b as e,c as i}from"./index.ee300dbb.js";var r=e(({app:a})=>{a.use(i())});export{r as default};
--- assets/pinia.e726a10f.js
+++
@@ -1,1 +0,0 @@
-import{b as e,c as i}from"./index.fbfb3097.js";var r=e(({app:a})=>{a.use(i())});export{r as default};
